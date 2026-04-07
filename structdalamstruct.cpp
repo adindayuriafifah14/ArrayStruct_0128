7ae0cd3d761e2eb727821cc09162d387525dbfb2
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-04-07 18:44:32 +0700

    Menambahkan isi data

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -16,4 +16,18 @@ int main ()
 {
     //deklarasi variabel struct
     orang mhs;
+    //mengisi data 
+    cout << "isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "isikan data desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << endl;
+
+    cout << "data tampil" << endl;
+    cout << "nama : " << mhs.nama << endl;
+    cout << "desa : " << mhs.alamat.desa << endl;
+    cout << "kota : " << mhs.alamat.kota << endl;
+    cout << "umur : " << mhs.umur << endl;
 }
