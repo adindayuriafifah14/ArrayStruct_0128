65f8c81f74777483fd1250ff121c59ed1c32cee5
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-04-07 18:05:36 +0700

    Menambahkan isi data

--- structarray.cpp
+++ structarray.cpp
@@ -12,4 +12,17 @@ int main()
 {
     //deklarasi variabel struct
     Orang mhs;
+    //mengisi data
+    cout << "isikan data nama : " ;
+    cin >> mhs.nama;
+    cout << "isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "data tampil" << endl;
+    cout << "nama : " << mhs.alamat << endl;
+    cout << "alamat : " << mhs.alamat <<endl;
+    cout << "umur : " << mhs.umur << endl;
 }
